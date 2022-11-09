c17def4fd2545fb1d82fe93023b148c95f11672e
fahrooo <fahrooo2808@student.uns.ac.id>
2022-11-09 13:22:35 +0700

    up

--- controller/Users.js
+++ controller/Users.js
@@ -95,15 +95,19 @@ export const Logout = async (req, res) => {
   if (!refreshToken) {
     return res.sendStatus(204);
   }
+  
   const user = await Users.findAll({
     where: {
       refresh_token: refreshToken,
     },
   });
+  
   if (!user[0]) {
     return res.sendStatus(204);
   }
+  
   const userId = user[0].id;
+  
   await Users.update(
     { refreshToken: null },
     {
@@ -112,6 +116,7 @@ export const Logout = async (req, res) => {
       },
     }
   );
+  
   res.clearCookie("refreshToken");
   return res.sendStatus(200);
 };
